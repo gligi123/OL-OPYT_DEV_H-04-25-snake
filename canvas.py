9374d6bac91328c2a0e7c12e4a3dc858e8c7e8c6
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-02-02 19:52:01 +0100

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -1,14 +1,17 @@
 import tkinter as tk
+import customtkinter as ctk
+
 
 STEP = 150
 
-root = tk.Tk()
+# root = tk.Tk()
+root = ctk.CTk()
 root.title('Tkinter canvas demo')
 
-lbl_title = tk.Label(root, text='Canvas demo', font=('Verdana', 18))
+lbl_title = ctk.CTkLabel(root, text='Canvas demo', font=('Verdana', 18))
 lbl_title.pack(pady=25)
 
-canvas = tk.Canvas(root, width=800, height=600, bg='black')
+canvas = ctk.CTkCanvas(root, width=800, height=600, bg='black')
 canvas.pack()
 
 canvas.create_text(10, 10, anchor='nw', fill='white', text='Tekst unutar Canvas widget-a')
